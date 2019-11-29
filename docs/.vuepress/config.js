8e185caf8e2aa3c29ce59a477210c7871556435b
zhouzh <363420113@qq.com>
2019-11-29 16:13:09 +0800

    添加目录及分类

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,33 @@
 module.exports = {
-    title: 'impact',
-    description: 'md docs',
-    base: '/impact-md/'
+    title: 'Garfield的个人空间',
+    description: 'Tech Otakus Save The World',
+    base: '/impact-md/',
+    markdown: {
+        lineNumbers: true // 代码行数
+    },
+    themeConfig: {
+        lastUpdated: '最后更新时间', // 最后更新时间
+        sidebarDepth: 3,
+        nav: [
+            {
+                text: 'javascript',
+                link: '/javascript/'
+            },{
+                text: 'css',
+                link: '/css/'
+            },{
+                text: 'react',
+                link: '/react/'
+            },{
+                text: 'tools',
+                link: '/tools/'
+            }
+
+        ],
+        sidebar:{
+            '/javascript/':[
+                '/javascript/JS对象',
+            ]
+        }
+    },
 };
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,16 @@
-# Hello VuePress !!!!
+---
+home: true
+actionText: 开始阅读 →
+actionLink: /
+heroImage: https://www.sobot.com/images/service/service_banner-b5e965bc53.png
+---
+
+- - -
+
+### 个人说明：
+
+#### 记录日常开发中的点滴历程
+#### 分享开发中的经验和心得
+#### 学习前沿的前端技术
+#### 回归初心完善基础知识和原生代码
+
